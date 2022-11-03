546a01feb89555a6da29277a796e44f2c7ef5e8d
Violetta <violetta.v.queen@gmail.com>
2022-11-03 10:14:31 -0600

    ticketing-project-data part2

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,8 +12,16 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
+        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+
         return null;
     }
 
